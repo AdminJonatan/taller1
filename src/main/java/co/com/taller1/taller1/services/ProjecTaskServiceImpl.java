1e2e5608a49a17b0ea6e6d011fd698d017c58358
AdminJonatan <Jonatan_gomez23191@elpoli.edu.co>
2022-02-09 18:20:18 -0500

    add config data source

--- src/main/java/co/com/taller1/taller1/services/ProjecTaskServiceImpl.java
+++ src/main/java/co/com/taller1/taller1/services/ProjecTaskServiceImpl.java
@@ -1,12 +1,12 @@
 package co.com.taller1.taller1.services;
 
 
-import co.com.taller1.taller1.entities.Backlog;
+
 import co.com.taller1.taller1.entities.ProjecTask;
 import co.com.taller1.taller1.repository.ProjectTaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
+
 
 import java.util.List;
 
